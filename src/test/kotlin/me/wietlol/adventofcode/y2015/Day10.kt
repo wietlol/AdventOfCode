d39129bffd1b7345bfda8e2cf0908aa74410bf93
Wietlol <harry@aaictconsultancy.nl>
2021-12-05 13:22:44 +0100

    add y2015 d10

---
+++ src/test/kotlin/me/wietlol/adventofcode/y2015/Day10.kt
@@ -0,0 +1,32 @@
+package me.wietlol.adventofcode.y2015
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Day10
+{
+	@Test
+	fun task1()
+	{
+		val answer = generateSequence("1113222113") { it.lookAndSay() }
+			.take(41)
+			.last()
+			.length
+		
+		assertEquals(252594, answer)
+	}
+	
+	@Test
+	fun task2()
+	{
+		val answer = generateSequence("1113222113") { it.lookAndSay() }
+			.take(51)
+			.last()
+			.length
+		
+		assertEquals(3579328, answer)
+	}
+	
+	fun String.lookAndSay(): String =
+		replace(Regex("(\\d)\\1*")) { it.value.length.toString() + it.value.first() }
+}
